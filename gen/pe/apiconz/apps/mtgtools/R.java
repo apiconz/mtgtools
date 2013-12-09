437c5cbe2065d7f0343460a750581b9c5c79c305
Armando <apiconz@gmail.com>
2013-12-09 18:04:59 -0500

    se arregló el splash screen y el aboutme activity

--- gen/pe/apiconz/apps/mtgtools/R.java
+++ gen/pe/apiconz/apps/mtgtools/R.java
@@ -24,6 +24,7 @@ public final class R {
         public static final int card_back=0x7f020000;
         public static final int ic_aboutme=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int ic_launcher_web=0x7f020003;
     }
     public static final class id {
         public static final int action_aboutme=0x7f08000d;
@@ -53,6 +54,10 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int aboutme_disclaimer=0x7f05000f;
+        public static final int aboutme_link=0x7f05000e;
+        public static final int aboutme_msg=0x7f05000d;
+        public static final int aboutme_title=0x7f05000c;
         public static final int action_aboutme=0x7f050002;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
