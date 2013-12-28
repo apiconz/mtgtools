23ee39c38348df31def808b8c26ea7cd395ec2ee
apiconz <apiconz@gmail.com>
2013-12-28 00:28:15 -0500

    va

--- src/pe/apiconz/apps/mtgtools/CounterActivity.java
+++ src/pe/apiconz/apps/mtgtools/CounterActivity.java
@@ -9,8 +9,26 @@ public class CounterActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		setContentView(R.layout.activity_counter);
 		super.onCreate(savedInstanceState);
-		
-		
+
 		NumberPicker numberPicker = (NumberPicker) findViewById(R.id.numberPicker1);
+
+		// String[] displayedValues = new String[140];
+		// int minValue = -40;
+		// for (int i = 0; i < displayedValues.length; i++) {
+		// displayedValues[i] = Integer.toString(minValue);
+		// minValue++;
+		// }
+
+		final int minValue = -50;
+		final int maxValue = 100;
+		numberPicker.setMinValue(0);
+		numberPicker.setMaxValue(maxValue - minValue);
+		numberPicker.setValue(1);
+		numberPicker.setFormatter(new NumberPicker.Formatter() {
+			@Override
+			public String format(int index) {
+				return Integer.toString(index - minValue);
+			}
+		});
 	}
 }
